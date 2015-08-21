84f66cde017f9e828ccbcb1c07f8c3016ef20429
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-08-20 22:32:19 -0700

    Very basic outline of the wrapper is done, doesn't properly pass through signals yet

--- cmd/boulder/main.go
+++ cmd/boulder/main.go
@@ -5,41 +5,172 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"syscall"
+
+	"github.com/letsencrypt/boulder/core"
+)
+
+type command struct {
+	help  string
+	where string
+}
 
 var (
-	services = map[string]string{
-		"ca":             "Handles issuance operations",
-		"ocsp-responder": "Handles OCSP requests",
-		"ra":             "Handles service orchestration",
-		"sa":             "Handles SQL operations",
-		"va":             "Handles challenge validation",
-		"wfe":            "Handles API requests",
-		"am":             "RPC activity monitor",
+	services = map[string]command{
+		"ca": command{
+			help: "Handles issuance operations",
+		},
+		"ocsp-responder": command{
+			help: "Handles OCSP requests",
+		},
+		"ra": command{
+			help: "Handles service orchestration",
+		},
+		"sa": command{
+			help: "Handles SQL operations",
+		},
+		"va": command{
+			help: "Handles challenge validation",
+		},
+		"wfe": command{
+			help: "Handles HTTP API requests",
+		},
+		"am": command{
+			help: "RPC activity monitor",
+		},
 	}
-	tools = map[string]string{
-		"revoker":                "Revoke issued certificates",
-		"expiration-mailer":      "Send certificate expiration emails",
-		"external-cert-importer": "Import external certificates for POP checks",
-		"ocsp-updater":           "Update/Generate OCSP responses",
+
+	tools = map[string]command{
+		"revoker": command{
+			help: "Revoke issued certificates",
+		},
+		"expiration-mailer": command{
+			help: "Send certificate expiration emails",
+		},
+		"external-cert-importer": command{
+			help: "Import external certificates for POP checks",
+		},
+		"ocsp-updater": command{
+			help: "Update/Generate OCSP responses",
+		},
 	}
 )
 
-func checkSystem() (map[string]string, error) {
-	return nil, nil
+func checkSystem(services, tools map[string]command) error {
+	// Find service binaries
+	for n := range services {
+		path, err := exec.LookPath(fmt.Sprintf("boulder-%s", n))
+		if err != nil {
+			delete(services, n)
+			continue
+		}
+		t := services[n]
+		t.where = path
+		services[n] = t
+	}
+
+	// Find tool binaries
+	for n := range tools {
+		path, err := exec.LookPath(fmt.Sprintf("boulder-%s", n))
+		if err != nil {
+			delete(tools, n)
+			continue
+		}
+		t := tools[n]
+		t.where = path
+		tools[n] = t
+	}
+
+	if len(services) == 0 && len(tools) == 0 {
+		return fmt.Errorf("Couldn't find any boulder binaries")
+	}
+
+	return nil
 }
 
-func printAvailable(services, tools map[string]string) {
-	fmt.Printf("Available commands\n# Services\n\n")
-	for name, help := range services {
-		fmt.Printf("\t%s -- %s\n", name, help)
+func printAvailable(services, tools map[string]command) {
+	if len(services) > 0 {
+		fmt.Printf("\n# Available commands\n[Services]\n")
+		for n, c := range services {
+			fmt.Printf("\t%s -- %s\n", n, c.help)
+		}
 	}
-	fmt.Printf("\n# Tools\n\n")
-	for name, help := range tools {
-		fmt.Printf("\t%s -- %s\n", name, help)
+	if len(tools) > 0 {
+		fmt.Printf("\n[Tools]\n")
+		for n, c := range tools {
+			fmt.Printf("\t%s -- %s\n", n, c.help)
+		}
+	}
+}
+
+func printHeader() {
+	fmt.Printf("usage: boulder [command] [subcommand] [flags]\n")
+	fmt.Printf("version: %s -- %s, Golang=(%s) BuildHost=(%s)\n", core.GetBuildID(), core.GetBuildTime(), runtime.Version(), core.GetBuildHost())
+	fmt.Printf("written by the Boulder contributors <ca-dev@letsencrypt.org\n")
+}
+
+func printFooter() {
+	fmt.Printf("\nFor more information about specific commands use 'boulder <command> help'\n")
+}
+
+func execute(name string, args []string) {
+	cmd, present := services[name]
+	if !present {
+		cmd, present = tools[name]
+		if !present {
+			fmt.Printf("Uknown command\n\n")
+			printHeader()
+			printAvailable(services, tools)
+			printFooter()
+			return
+		}
+	}
+	e := exec.Command(cmd.where, args...)
+	e.Env = os.Environ()
+	eReader, err := e.StdoutPipe()
+	if err != nil {
+		// BAD
+		return
+	}
+	go func() {
+		scanner := bufio.NewScanner(eReader)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+	if err := e.Run(); err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+
+			// This works on both Unix and Windows. Although package
+			// syscall is generally platform dependent, WaitStatus is
+			// defined for both Unix and Windows and in both cases has
+			// an ExitStatus() method with the same signature.
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				os.Exit(status.ExitStatus())
+			}
+		} else {
+			log.Fatalf("Failed to execute [%s %s]: %v", cmd.where, strings.Join(args, " "), err)
+		}
 	}
 }
 
 func main() {
+	checkSystem(services, tools)
+	if len(os.Args) < 2 || len(os.Args) >= 2 && os.Args[1] == "help" {
+		printHeader()
+		printAvailable(services, tools)
+		printFooter()
+		return
+	}
 
+	execute(os.Args[1], os.Args[2:])
 }
