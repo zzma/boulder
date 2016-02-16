f9966dc7225dda702acf19b8290bfa06a356c97e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-02-15 21:13:17 -0800

    Add a 'godep restore' test in Travis.
    This ensures our godeps match the remote repo.

--- test.sh
+++ test.sh
@@ -7,7 +7,9 @@ fi
 
 # The list of segments to run. To run only some of these segments, pre-set the
 # RUN variable with the ones you want (see .travis.yml for an example).
-# Order doesn't matter.
+# Order doesn't matter. Note: godep-restore is specifically left out of the
+# defaults, because we don't want to run it locally (would be too disruptive to
+# GOPATH).
 RUN=${RUN:-vet lint fmt migrations unit integration}
 
 # The list of segments to hard fail on, as opposed to continuing to the end of
@@ -273,6 +275,16 @@ if [[ "$RUN" =~ "integration" ]] ; then
       ;;
   esac
   end_context #integration
+fi
+
+# Run godep-restore (happens only in Travis) to check that the hashes in
+# Godeps.json really exist in the remote repo and match what we have.
+if [[ "$RUN" =~ "godep-restore" ]] ; then
+  start_context "godep-restore"
+  run_and_comment godep restore
+  run_and_comment godep save -r ./...
+  run_and_comment git diff --exit-code
+  end_context #godep-restore
 fi
 
 exit ${FAILURE}
