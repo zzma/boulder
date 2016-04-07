dee1d740922627b17d7bc5eaf61de349ab04a6f5
Kane York <rikingcoding@gmail.com>
2016-04-07 10:40:03 -0700

    remove forking from setup script

--- test/setup.sh
+++ test/setup.sh
@@ -4,6 +4,8 @@
 # and configure database and RabbitMQ.
 #
 
+set -e
+
 go get \
   golang.org/x/tools/cmd/vet \
   golang.org/x/tools/cmd/cover \
@@ -12,16 +14,16 @@ go get \
   github.com/mattn/goveralls \
   github.com/modocache/gover \
   github.com/jcjones/github-pr-status \
-  github.com/jsha/listenbuddy &
+  github.com/jsha/listenbuddy #&
 
 (wget https://github.com/jsha/boulder-tools/raw/master/goose.gz &&
  mkdir -p $GOPATH/bin &&
  zcat goose.gz > $GOPATH/bin/goose &&
  chmod +x $GOPATH/bin/goose &&
- ./test/create_db.sh) &
+ ./test/create_db.sh) #&
 
 # Set up rabbitmq exchange
-go run cmd/rabbitmq-setup/main.go -server amqp://boulder-rabbitmq &
+go run cmd/rabbitmq-setup/main.go -server amqp://boulder-rabbitmq #&
 
 # Wait for all the background commands to finish.
 wait
